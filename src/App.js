07a01999cbb0feef5f7bfc3543783acb8386c0e6
mohamdallam <mohamedahmedallam1@gmail.com>
2022-04-23 15:28:27 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "./index.css";
+import { person } from "./date";
 
 function App() {
   return (
     <div className="font color-body">
       <Container className="py-5">
         <Row className="justify-content-center">
-          <Col sm="8">You have appointments today</Col>
+          <Col sm="8">You have appointments today {person.length}</Col>
         </Row>
 
-        <Row className="justify-content-center">
+        <Row className="justify-content-center my-4">
           <Col sm="8">
-            <div className="rectangle"></div>
+            <div className="rectangle p-2">
+              {person.length ? (
+                person.map((item) => {
+                  return (
+                    <div className="d-flex border-bottom mx-3 my-3">
+                      <img
+                        className="img-avatar"
+                        src="person1.PNG"
+                        alt="img"
+                      ></img>
+                      <div className="px-3">
+                        <p className="d-inline">{item.name}</p>
+                        <p className="">{item.date}</p>
+                      </div>
+                    </div>
+                  );
+                })
+              ) : (
+                <h2>No Appointments Today</h2>
+              )}
+            </div>
           </Col>
         </Row>
 
---
+++ src/date.js
@@ -0,0 +1,20 @@
+export const person = [
+  {
+    id: "1",
+    name: "ahmed",
+    date: "5pm",
+    img: "person1.PNG",
+  },
+  {
+    id: "2",
+    name: "mohamed",
+    date: "2pm",
+    img: "person2.PNG",
+  },
+  {
+    id: "3",
+    name: "mostafa",
+    date: "8pm",
+    img: "person3.PNG",
+  },
+];
